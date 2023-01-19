325b92e7eb56c34e5d6b7008e397c7b22ef1d0f2
Lewiix <kamil.lewandowski998@gmail.com>
2023-01-19 23:50:34 +0100

    Add code in script.js get href, find correct article, add class active

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked');
     console.log(event);
@@ -9,7 +10,7 @@ function titleClickHandler(event) {
     for (let activeLink of activeLinks ) {
         activeLink.classList.remove('active');
     }
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
     console.log('clcikedElement:', clickedElement);
     clickedElement.classList.add('active');
     
@@ -19,11 +20,15 @@ function titleClickHandler(event) {
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+    const hrefAttribute = clickedElement.getAttribute('href');
+    console.log (hrefAttribute);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const valueSelector = document.querySelector(hrefAttribute);
+    console.log(valueSelector);
+  /* [DONE] add class 'active' to the correct article */
+    valueSelector.classList.add('active');
 }
 const links = document.querySelectorAll('.titles a')
 for (let link of links) {
